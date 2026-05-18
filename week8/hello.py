9c2e72f5513da5eff98d829c1ac5baff7092ceaa
pranavrai070 <pranavrai070@gmail.com>
2026-05-18 11:08:04 +0530

    fetch fresh

--- week8/hello.py
+++ week8/hello.py
@@ -22,8 +22,29 @@ def hello() -> str:
 # New - added thanks to student Tue H.!
 
 
-@app.function(image=image, region="eu")
+@app.function(image=image, region="au")
 def hello_europe() -> str:
+    import requests
+
+    response = requests.get("https://ipinfo.io/json")
+    data = response.json()
+    city, region, country = data["city"], data["region"], data["country"]
+    return f"Hello from {city}, {region}, {country}!!"
+
+
+@app.function(image=image, region="ap-south")
+def hello_india() -> str:
+    import requests
+
+    response = requests.get("https://ipinfo.io/json")
+    data = response.json()
+    city, region, country = data["city"], data["region"], data["country"]
+    return f"Hello from {city}, {region}, {country}!!"
+
+
+
+@app.function(image=image, region="au")
+def hello_aus() -> str:
     import requests
 
     response = requests.get("https://ipinfo.io/json")
